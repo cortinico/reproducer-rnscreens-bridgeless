5eaa7f7d18b6516924598ea4ef5210663967b3ad
Nicola Corti <corti.nico@gmail.com>
2024-01-26 14:40:13 +0000

    Attempt to reproduce react-native-screens crash

--- App.tsx
+++ App.tsx
@@ -25,6 +25,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import {Screen, ScreenStack} from 'react-native-screens';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -64,34 +66,11 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <ScreenStack>
+        <Screen>
+          <Header />
+        </Screen>
+      </ScreenStack>
     </SafeAreaView>
   );
 }
